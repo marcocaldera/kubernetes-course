4155970ad6b09b84789e3f87844c5ead5ca66e8e
Marco Caldera <marco.caldera@enhancers.it>
2021-08-29 17:42:04 +0200

    update

--- 01.kube-first/app.js
+++ 01.kube-first/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app!!!!</h1>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
 
-app.get('/error', (req, res) => {
+app.get("/error", (req, res) => {
   process.exit(1);
 });
 
